1bebff6d56c7daf83000000e943f4306c63b9ff2
Dmitry <deorditse.d@yandex.com>
2023-05-29 15:38:56 +0300

    new

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,16 @@ import 'package:intl/date_symbol_data_local.dart';
 import 'package:models/models.dart';
 import 'package:obrix/ui_layout/pages/pages.dart';
 import 'package:obrix/ui_layout/widgets_for_all_pages/src/styles/styles.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 //flutter build web --web-renderer canvaskit
 Future<void> main() async {
   //для экрана заставки
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   // FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   await initializeDateFormatting();
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
